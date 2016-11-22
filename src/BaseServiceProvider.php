7b146ee6bfdb5b90677c713234cb54bdf4e2091d
Kyle Nieber <kyle.nieber@gmail.com>
2016-11-22 16:07:58 -0500

    update register

--- src/BaseServiceProvider.php
+++ src/BaseServiceProvider.php
@@ -12,7 +12,7 @@ class BaseServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->singleton('marketo', function() {
+        $this->app->singleton(Client::class, function() {
            return new Client([
                'api_url' => config('marketo.auth.api_url'),
                'client_id' => config('marketo.auth.client_id'),
